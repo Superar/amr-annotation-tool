2a566c05d86033f7e2e1a896db836527abe0eae0
Marcio Lima <marcio.lima.inacio@gmail.com>
2019-10-31 18:08:44 -0300

    Adicionar etiqueta aos nós

--- js/app.js
+++ js/app.js
@@ -8,16 +8,31 @@ var data = {
 };
 
 var options = {
-    physics: false
+    physics: false,
+    manipulation: {
+        enabled: false,
+        editNode: editNode
+    }
 };
 
 var network = new vis.Network(container, data, options);
+var selectedNode;
 
 network.on('doubleClick', function (event) {
     var x = event.pointer.canvas.x,
         y = event.pointer.canvas.y;
-    nodes.update({ x: x, y: y });
+    var node = network.getNodeAt(event.pointer.DOM);
+    if (!node) {
+        nodes.update({ x: x, y: y, label: 'tete' });
+    } else {
+        network.editNode();
+    }
 });
+
+network.on('deselectNode', function (event) {
+    var input = document.getElementById('txt-edit');
+    input.style.visibility = 'hidden';
+})
 
 document.addEventListener('keydown', function (event) {
     if (event.key == 'Shift') {
@@ -29,3 +44,26 @@ document.addEventListener('keyup', function (event) {
         network.disableEditMode();
     }
 });
+
+document.addEventListener('keypress', function (event) {
+    if (event.key == 'Enter') {
+        var input = document.getElementById('txt-edit');
+        input.style.visibility = 'hidden';
+    }
+});
+
+function editNode(data, callback) {
+    var input = document.getElementById('txt-edit');
+    input.style.left = (data.x + 200) + 'px';
+    input.style.top = (data.y + 200) + 'px';
+    input.style.visibility = 'visible';
+    input.value = data.label;
+    container.appendChild(input);
+
+    selectedNode = data;
+    input.oninput = function () {
+        node = nodes.update({ id: network.getSelectedNodes()[0], label: input.value });
+    };
+
+    callback(data);
+}
